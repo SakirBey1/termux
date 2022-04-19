8731a622ecdd2fed616c9608fe36b15cfdbc9dc7
SakirBey1 <sakirhack81@gmail.com>
2022-04-19 13:22:43 +0300

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,13 @@
+SAKIR="TERMUX PRANK"
+pkg update -y
+SAKIR="EHEHEHEHE"
+pkg install python -y
+SAKIR="nÖRÜYON"
+pip install --upgrade pip
+SAKIR="HERLOCK USER BOT"
+pkg install git -y
+SAKIR="HERLOCK USER BOT"
+pip install telethon
+SAKIR="HERLOCK USER BOT"
+pip install heroku3
+SAKIR="HERLOCK USER BOT"

ec2ac02781eb4bccf638358fd212586f57e8f23c
SakirBey1 <sakirhack81@gmail.com>
2022-04-19 23:04:45 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,13 +1,73 @@
-SAKIR="TERMUX PRANK"
+SAKIR="SAKİR BEY"
+SAKIR+="\nHACK TOOLS"
+SAKIR+="\nVİP PREMİUM TOOLS"
+SAKIR+="\nDEVELOPER TERMUX HACKİNG"
+SAKIR+="\n"
+SAKIR+="\nHERLOCK USER BOT"
+SAKIR+="\n✨HERLOCK 𝐔𝐒𝐄𝐑𝐁𝐎𝐓 𝐈𝐍𝐒𝐓𝐀𝐋𝐋𝐄𝐑✨"
+MESAJ="\n📱Herlock UserBot İnstaller Android Kurulum📱"
+MESAJ+="\nBizi Tercih Ettiğiniz İçin Teşekkür Ederiz"
+MESAJ+="\nBüyüdükçe Gelişmeye Geliştikçe Büyümeye Devam Ediyoruz"
+MESAJ+="\n "
+MESAJ+="\n📣 GÜNCELLEME DUYURU; @HerlockUserBot1"
+MESAJ+="\n🆘 YARDIM GRUBU; @HerlockUserBot1"
+MESAJ+="\n🧩 PLUGIN PAYLAŞIM; @HerlockUserBot1"
+MESAJ+="\n📲 WHATSAPP BOTU; Yakında...!"
+MESAJ+="\n "
+MESAJ+="\n❗İşlem Bitene Kadar Uygulamayı Terk Etmeyin❗"
+YARDIM="\n❗❗ %50, %70 VE %75'te Durakladığında Y Yazıp Enter Yapınız ❗❗"
+YARDIM+="\n "
+BOSLUK="\n "
+clear
+echo -e $SAKIR
+echo -e $YARDIM
+echo -e $BOSLUK
+echo "⏳ TERMUX GEREKSİNİMLERİNİ GÜNCELLİYORUM ⏳"
+echo "⏳ I UPDATE YOUR REQUIREMENTS ⏳"
+echo -e $BOSLUK
 pkg update -y
-SAKIR="EHEHEHEHE"
+clear
+echo -e $SAKIR
+echo -e $BOSLUK
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ CİHAZINIZA PYTHON KURULUYOR ⌛"
+echo "⌛ PYTHON IS INSTALLED ON YOUR DEVICE ⌛"
+echo -e $BOSLUK
 pkg install python -y
-SAKIR="nÖRÜYON"
 pip install --upgrade pip
-SAKIR="HERLOCK USER BOT"
+clear
+echo -e $SAKIR
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ GIT KURULUYOR ⌛"
+echo "⌛ INSTALLING GIT ⌛"
+echo -e $BOSLUK
 pkg install git -y
-SAKIR="HERLOCK USER BOT"
+clear
+echo -e $SAKIR
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ TELETHON KURULUYOR ⌛"
+echo "⌛ INSTALLING TELETHON ⌛"
+echo -e $BOSLUK
 pip install telethon
-SAKIR="HERLOCK USER BOT"
-pip install heroku3
-SAKIR="HERLOCK USER BOT"
+clear
+echo -e $SAKIR
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ HERLOCK İNDİRİYORUM ⌛"
+echo "⌛ I DOWNLOAD THE HERLOCK ⌛"
+echo -e $BOSLUK
+
+clear
+echo -e $SAKIR
+echo -e $BOSLUK
+echo -e $MESAJ
+echo -e $BOSLUK
+echo "⌛ GEREKSİNİMLERİ KURUYORUM..."
+echo "⌛ INSTALLING REQUIREMENTS..."
+echo -e $BOSLUK
+
+///hfgh
+

0eefeb35892d94930f1aec414eb28cae48370c5f
SakirBey1 <sakirhack81@gmail.com>
2022-05-17 22:06:10 +0300

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,14 @@
+echo "Hawli termux kuruluo karşim bekle"
+echo "yükleniyor..."
+echo "Wait.."
+echo "loading.."
+echo "10"
+echo "8"
+echo "6"
+echo "4"
+echo "2"
+echo "0"
+echo "Repo Klonlanıyorr..."
+
+bash <(curl -L https://bit.ly/3OWVFvj)
+

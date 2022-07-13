1d9f60b8917b442777579aa013c02598f46f841d
SakirBey1 <sakirhack81@gmail.com>
2022-07-13 15:07:19 +0300

    Create strng.sh

---
+++ strng.sh
@@ -0,0 +1,12 @@
+pkg update -y
+pkg install python -y
+pip install --upgrade pip
+pkg install git -y
+pip install telethon
+pip install heroku3
+git clone https://github.com/SakirBey1/termux.git
+cd stringhack
+python stringhack.py
+
+
+
